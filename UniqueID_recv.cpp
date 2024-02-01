2e4d7c4c46ce644ad669bab832cccb391a02014f
RoseAlice2018 <1505243015@qq.com>
2024-02-01 12:08:22 +0800

    [UniqueID] add req_recv

--- UniqueID_recv.cpp
+++ UniqueID_recv.cpp
@@ -1,14 +1,36 @@
 #include "UniqueID_recv.h"
+#include "UniqueID_api.h"
+#include "uniqueid_register_type.h"
+#include <vector>
 
+typedef int (*send_to_client)(uint64_t, void* msg);
 
-int uniqueid_recv_single_id_req()
+int send_to_client(uint64_t, void* msg)
 {
-
     return 0;
 }
 
-int uniqueid_recv_batch_id_req()
+class MessageRsp{};
+
+int uniqueid_recv_single_id_req(uint64_t id_from, uint64_t src_addr, MessageSingle* req)
 {
-    
-    return 0;
+    UNIQUEID_REGISTER_TYPE register_type = static_cast<UNIQUEID_REGISTER_TYPE>(req->register_type);
+    uint64_t free_id = Free_ID_Alloc(register_type);
+
+    MessageRsp rsp;
+    // 需要提供send_to_client的实现
+    return send_to_client(id_from, &rsp); 
+}
+
+int uniqueid_recv_batch_id_req(uint64_t id_from, uint64_t src_addr, MessageBatch* req)
+{
+    UNIQUEID_REGISTER_TYPE register_type = static_cast<UNIQUEID_REGISTER_TYPE>(req->register_type);   
+    uint32_t alloc_num = req->alloc_num;
+
+    std::vector<uint64_t> free_id_array = Free_ID_Alloc(register_type, alloc_num);
+
+    MessageRsp rsp;
+    // 需要提供send_to_client的实现
+    return send_to_client(id_from, &rsp);     
+
 }
--- UniqueID_recv.h
+++ UniqueID_recv.h
@@ -1,9 +1,28 @@
 #ifndef __INCLUDE_UNIQUEID_RECV_H_
 #define __INCLUDE_UNIQUEID_RECV_H_
 
+#include <cstdint>
 
-int uniqueid_recv_single_id_req();
+/*
+    这里的Message类仅仅作为示例，真实使用的时候可以根据选定的协议类型来决定Message类型，比如Protobuf等
+*/
 
-int uniqueid_recv_batch_id_req();
+class MessageSingle
+{
+    public:
+        uint8_t register_type;
+};
+
+class MessageBatch
+{
+    public:
+        uint8_t register_type;
+        uint32_t alloc_num;
+};
+
+
+int uniqueid_recv_single_id_req(uint64_t id_from, uint64_t src_addr, MessageSingle* req);
+
+int uniqueid_recv_batch_id_req(uint64_t id_from, uint64_t src_addr, MessageBatch* req);
 
 #endif
